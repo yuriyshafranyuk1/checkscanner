b9061b1b0cc99a5d47e0ceeed9af3e68985af63a
Yuriy Shafranyuk <Yuriy_Shafranyuk@epam.com>
2016-08-09 13:07:38 +0300

    Added basic project.

---
+++ src/main/java/com/checkscanner/check/CheckApplication.java
@@ -0,0 +1,12 @@
+package com.checkscanner.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckApplication.class, args);
+	}
+}
