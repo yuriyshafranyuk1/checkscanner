abd43b8d7d3c3c1bd9819742b44d9fb8b2dc072c
Yuriy Shafranyuk <Yuriy_Shafranyuk@epam.com>
2016-08-09 13:55:39 +0300

    Added empty data loader.

--- src/main/java/com/checkscanner/check/CheckApplication.java
+++ src/main/java/com/checkscanner/check/CheckApplication.java
@@ -1,12 +1,19 @@
 package com.checkscanner.check;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan(basePackages = "com.checkscanner.check")
+@EnableAutoConfiguration
 @SpringBootApplication
-public class CheckApplication {
+public class CheckApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(final String[] args)
+	{
 		SpringApplication.run(CheckApplication.class, args);
 	}
 }
---
+++ src/main/java/com/checkscanner/check/DataLoader.java
@@ -0,0 +1,17 @@
+package com.checkscanner.check;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DataLoader
+{
+
+	@PostConstruct
+	public void loadSomeData()
+	{
+
+	}
+}
