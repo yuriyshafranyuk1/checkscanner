17fb7f21f649de634596a1080ae60533aa2ab1ec
Yuriy Shafranyuk <Yuriy_Shafranyuk@epam.com>
2016-08-09 14:14:18 +0300

    Added code with one bug.

--- src/main/java/com/checkscanner/check/DataLoader.java
+++ src/main/java/com/checkscanner/check/DataLoader.java
@@ -1,8 +1,17 @@
 package com.checkscanner.check;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 @Component
@@ -12,6 +21,31 @@ public class DataLoader
 	@PostConstruct
 	public void loadSomeData()
 	{
+		final File dir = new File("src/main/resources/venues/");
+		final File[] directoryListing = dir.listFiles();
+		final int count = 0;
+
+		if (directoryListing != null)
+		{
+
+			for (final File child : directoryListing)
+			{
+				try
+				{
+					final Scanner scanner = new Scanner(child);
+					final String content = scanner.useDelimiter("\\Z").next();
+
+					final Gson gson = new GsonBuilder().create();
+					final Map venue = gson.fromJson(content, Map.class);
+
+					final List<Map> placeCategories = (List<Map>) venue.get("categories");
+					scanner.close();
+				}
+				catch (final FileNotFoundException e)
+				{
+				}
+			}
+		}
 
 	}
 }
