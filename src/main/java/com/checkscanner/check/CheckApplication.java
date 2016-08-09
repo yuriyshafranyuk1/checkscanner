b7b872161a2cd2110a28602ea18d1a9ba5148eaa
yshafranyuk <yshafranyuk@bitbucket.org>
2016-08-09 23:31:46 +0300

    fixed all sonar issues

--- src/main/java/com/checkscanner/check/CheckApplication.java
+++ src/main/java/com/checkscanner/check/CheckApplication.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import java.util.Map;
 
 @ComponentScan(basePackages = "com.checkscanner.check")
 @EnableAutoConfiguration
 @SpringBootApplication
-public class CheckApplication
-{
+public class CheckApplication {
 
-	public static void main(final String[] args)
-	{
+	public static void main(final String[] args) {
 		SpringApplication.run(CheckApplication.class, args);
 	}
 }
