7132db4c2c3ae85225a900ef84aa18a32446d30b
Yuriy Shafranyuk <Yuriy_Shafranyuk@epam.com>
2016-08-10 12:41:17 +0300

    added unused import.

--- src/main/java/com/checkscanner/check/DataLoader.java
+++ src/main/java/com/checkscanner/check/DataLoader.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
+import java.util.Set;
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
